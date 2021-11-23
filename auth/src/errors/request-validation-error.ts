8066c4b003a1a861db16857b46428d295e74cbe8
fahmi ahmad <fahmiahmad@tutanota.com>
2021-11-23 20:44:41 +0700

    require auth fixed

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,19 +1,19 @@
-import { ValidationError } from "express-validator";
-import { CustomError } from "./custom-error";
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
 export class RequestValidationError extends CustomError {
   statusCode = 400;
 
   constructor(public errors: ValidationError[]) {
-    super("Invalid request parameters");
+    super('Invalid request parameters');
 
-    // only because we are extending a builtin class
+    // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
 
   serializeErrors() {
-    return this.errors.map((error) => {
-      return { message: error.msg, field: error.param };
+    return this.errors.map(err => {
+      return { message: err.msg, field: err.param };
     });
   }
 }
