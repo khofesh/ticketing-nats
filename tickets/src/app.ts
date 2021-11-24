fbb8cb5d455b73bfe85ce6df610301daaf24a69b
fahmi ahmad <fahmiahmad@tutanota.com>
2021-11-24 10:34:59 +0700

    routes, model, and tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,15 @@ import express from "express";
 import "express-async-errors";
 import cookieSession from "cookie-session";
 
-import { errorHandler, NotFoundError } from "@slipperyslope/common";
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from "@slipperyslope/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
 
 const app = express();
 app.set("trust proxy", true);
@@ -13,6 +21,12 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
